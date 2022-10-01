bcac006c480ae99650db0377ed6949e4d1e93fe3
Ogunyomi Toluwanimi <odinprojectdevs@gmail.com>
2022-10-02 00:28:06 +0100

    added geolocation and fixed toggle button

--- src/index.js
+++ src/index.js
@@ -24,13 +24,29 @@ const domManipulation = (() => {
 })();
 
 function toggleTemp(temp1, temp2, moreData) {
+  if (domManipulation.togggleFarenheit.classList.contains('check')) {
+    console.log('check');
+    var firstTemp = Math.round((temp1 - 273.1) * (9 / 5) + 32);
+    var secondTemp = Math.round((temp2 - 273.1) * (9 / 5) + 32);
+    domManipulation.temperature.innerHTML = `${firstTemp}°F`;
+    domManipulation.weatherStatus.innerHTML = `Feels like ${secondTemp}°F ${moreData}`;
+  } else {
+    firstTemp = Math.round(temp1 - 273.15);
+    secondTemp = Math.round(temp2 - 273.15);
+    domManipulation.temperature.innerHTML = `${firstTemp}°C`;
+    domManipulation.weatherStatus.innerHTML = `Feels like ${secondTemp}°C ${moreData}`;
+  }
   domManipulation.togggleFarenheit.addEventListener('change', (event) => {
     if (event.target.checked) {
-      var firstTemp = Math.round((temp1 - 273.1) * (9 / 5) + 32);
-      var secondTemp = Math.round((temp2 - 273.1) * (9 / 5) + 32);
+      event.target.classList.add('check');
+      firstTemp = Math.round((temp1 - 273.1) * (9 / 5) + 32);
+      secondTemp = Math.round((temp2 - 273.1) * (9 / 5) + 32);
       domManipulation.temperature.innerHTML = `${firstTemp}°F`;
       domManipulation.weatherStatus.innerHTML = `Feels like ${secondTemp}°F ${moreData}`;
     } else {
+      if (event.target.classList.contains('check')) {
+        event.target.classList.remove('check');
+      }
       firstTemp = Math.round(temp1 - 273.15);
       secondTemp = Math.round(temp2 - 273.15);
       domManipulation.temperature.innerHTML = `${firstTemp}°C`;
@@ -45,11 +61,11 @@ function formatTime(suntime) {
     minute: '2-digit',
   });
 }
-async function getWeatherDetails(location) {
+async function getWeatherDetails(location1, location2) {
   domManipulation.secondappContainer.innerHTML = 'Loading....';
   const inputValue = domManipulation.inputBox.value;
-  if (location) {
-    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=0cdd23bc9251bc7da62c183bfc86ffde`, { mode: 'cors' });
+  if (location1 && location2) {
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location1}&lon=${location2}&appid=0cdd23bc9251bc7da62c183bfc86ffde`, { mode: 'cors' });
   } else {
     response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&APPID=0cdd23bc9251bc7da62c183bfc86ffde`, { mode: 'cors' });
   }
@@ -58,9 +74,6 @@ async function getWeatherDetails(location) {
   const remWeatherData = ` | ${weatherObject.weather[0].description} | Humidity ${weatherObject.main.humidity}%`;
   toggleTemp(weatherObject.main.temp, weatherObject.main.feels_like, remWeatherData);
   domManipulation.location.innerHTML = `${weatherObject.name}, ${weatherObject.sys.country}`;
-  domManipulation.temperature.innerHTML = `${Math.round(`${weatherObject.main.temp}` - 273.15)}°C`;
-  domManipulation.weatherStatus.innerHTML = `Feels like ${Math.round(`${weatherObject.main.feels_like
-  }` - 273.15)}°C${remWeatherData}`;
   domManipulation.weatherStatusImage.src = `https://openweathermap.org/img/wn/${weatherObject.weather[0].icon}@2x.png`;
   const sunrise = formatTime(weatherObject.sys.sunrise);
   const sunset = formatTime(weatherObject.sys.sunset);
@@ -74,7 +87,13 @@ domManipulation.displayWeatherDetailsBtn.addEventListener('click', () => {
     domManipulation.secondappContainer.innerHTML = 'Error, location not found';
   });
 });
-getWeatherDetails('magboro')
-  .catch(() => {
-    domManipulation.secondappContainer.innerHTML = 'Error, location not found';
+
+function getUserLocation() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    getWeatherDetails(Math.round(position.coords.latitude), Math.round(position.coords.longitude))
+      .catch(() => {
+        domManipulation.secondappContainer.innerHTML = 'Error, location not found';
+      });
   });
+}
+getUserLocation();
