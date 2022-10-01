c29dadcadda8f7635b2a23aa6dd2dc298be40cfa
Ogunyomi Toluwanimi <odinprojectdevs@gmail.com>
2022-10-01 23:52:42 +0100

    fixed https protocol

--- src/index.js
+++ src/index.js
@@ -49,9 +49,9 @@ async function getWeatherDetails(location) {
   domManipulation.secondappContainer.innerHTML = 'Loading....';
   const inputValue = domManipulation.inputBox.value;
   if (location) {
-    var response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=0cdd23bc9251bc7da62c183bfc86ffde`, { mode: 'cors' });
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=0cdd23bc9251bc7da62c183bfc86ffde`, { mode: 'cors' });
   } else {
-    response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputValue}&APPID=0cdd23bc9251bc7da62c183bfc86ffde`, { mode: 'cors' });
+    response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&APPID=0cdd23bc9251bc7da62c183bfc86ffde`, { mode: 'cors' });
   }
   const weatherObject = await response.json();
   // displaying details
